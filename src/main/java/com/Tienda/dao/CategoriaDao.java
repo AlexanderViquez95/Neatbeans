c40e11a42374965d38fc8c7544eb29cf44189a43
alexj <alexj@Alexander95>
2023-07-12 20:55:26 -0600

    Semana9

--- src/main/java/com/Tienda/dao/CategoriaDao.java
+++ src/main/java/com/Tienda/dao/CategoriaDao.java
@@ -2,9 +2,13 @@
 package com.Tienda.dao;
 
 import com.Tienda.domain.Categoria;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface CategoriaDao extends JpaRepository<Categoria, Long>{
+   
+
+    public List<Categoria> findByDescripcion(String descripcion);
     
 }
